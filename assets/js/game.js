a2e516c5c1beae268dc3fda1c7df32135c6dedc6
Juan Gómez Carrillo <gomezcarrillojuan@gmail.com>
2020-10-03 22:25:16 +0200

    New game button and victory message working

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,116 +1,140 @@
-let deck           = [];
-const suits        = ['C','D','H','S'];
-const specialCards = ['A','J','Q','K'];
+let deck = [];
+const suits = ["C", "D", "H", "S"];
+const specialCards = ["A", "J", "Q", "K"];
 
-let playerScore   = 0,
-    computerScore = 0
+let playerScore = 0,
+  computerScore = 0;
 
 // HTML References
-const btnTakeCard      = document.querySelector('#btnTakeCard');
-const btnStopPlaying   = document.querySelector('#btnStopPlaying');
-const scores           = document.querySelectorAll('small');
-const playerCardsDiv   = document.querySelector('#player-cards');
-const computerCardsDiv = document.querySelector('#computer-cards');
+const btnTakeCard = document.querySelector("#btnTakeCard");
+btnTakeCard.disabled = true; // Button disabled on game start.
+const btnStopPlaying = document.querySelector("#btnStopPlaying");
+btnStopPlaying.disabled = true; // Button disabled on game start.
+const btnNewGame = document.querySelector("#btnNewGame");
+
+
+const scores = document.querySelectorAll("small");
+const playerCardsDiv = document.querySelector("#player-cards");
+const computerCardsDiv = document.querySelector("#computer-cards");
 
 // Function that creates a new shuffled deck.
 const createDeck = () => {
-
-  for ( let i = 2; i <= 10; i++ ) {
-    for( let suit of suits ) {
-      deck.push( i + suit );
+  for (let i = 2; i <= 10; i++) {
+    for (let suit of suits) {
+      deck.push(i + suit);
     }
   }
 
-  for ( let suit of suits ) {
-    for ( let specialCard of specialCards ) {
-      deck.push( specialCard + suit );
+  for (let suit of suits) {
+    for (let specialCard of specialCards) {
+      deck.push(specialCard + suit);
     }
   }
 
-  deck = _.shuffle( deck );
+  deck = _.shuffle(deck);
 
   return deck;
-}
+};
+
+// Function that initializes the game.
+const startGame = (players = 2) => {
+
+  deck = createDeck();
+
+  playersScore = [];
+  for (let i = 0; i < players; i++) {
+    playersScore.push(0);
+  }
+
+  scores.forEach((elem) => (elem.innerText = 0));
+  playerScore = 0;
+  computerScore = 0;
+  playerCardsDiv.innerHTML = "";
+  computerCardsDiv.innerHTML = "";
+
+  btnTakeCard.disabled = false;
+  btnStopPlaying.disabled = false;
+};
 
 // Function to take a card from the deck.
 const takeCard = () => {
-
-  if ( deck.length === 0 ) {
-    throw 'There are no cards on the deck';
+  if (deck.length === 0) {
+    throw "There are no cards on the deck";
   }
 
   const card = deck.pop();
   return card;
-}
-
+};
 
 // Function checking the card's value
-const cardValue = ( card ) => {
+const cardValue = (card) => {
+  const value = card.substring(0, card.length - 1);
 
-  const value  = card.substring(0, card.length - 1);
-
-  return ( isNaN( value )) ?
-         ( value === 'A' ) ? 11 : 10
-         : parseInt( value );
-}
+  return isNaN(value) ? (value === "A" ? 11 : 10) : parseInt(value);
+};
 
 // Computer's turn
-const computersTurn = ( minimumPoints ) => {
-
+const computersTurn = (minimumPoints) => {
   do {
     // Take card from deck.
     const card = takeCard();
-    computerScore += cardValue( card );
+    computerScore += cardValue(card);
     // Display score on HTML.
     scores[1].innerText = computerScore;
-  
+
     // Create card and append it to HTML.
-    const cardImg = document.createElement('img');
+    const cardImg = document.createElement("img");
     cardImg.src = `./assets/cards/${card}.png`;
-    cardImg.classList.add('blackjack-card');
-    computerCardsDiv.append( cardImg );
+    cardImg.classList.add("blackjack-card");
+    computerCardsDiv.append(cardImg);
 
-    if ( minimumPoints > 21 ) { break; }
+    if (minimumPoints > 21) {
+      break;
+    }
+  } while (computerScore < minimumPoints && minimumPoints <= 21);
 
-  } while ( (computerScore < minimumPoints) && (minimumPoints <= 21) );
-
-  ( computerScore === playerScore ) ? alert('Tie!') :
-  ( playerScore > 21 ) ? alert('Computer wins!') : 
-  ( computerScore > playerScore && computerScore <= 21) ? alert('Computer wins!') :
-  alert('You won!');
-}
- 
-createDeck();
+  setTimeout(() => {
+    computerScore === playerScore
+      ? alert("Tie!")
+      : playerScore > 21
+      ? alert("Computer wins!")
+      : computerScore > playerScore && computerScore <= 21
+      ? alert("Computer wins!")
+      : alert("You won!");
+  }, 100);
+};
 
 // Eventos
-btnTakeCard.addEventListener('click', () => {
-  
+btnTakeCard.addEventListener("click", () => {
   // Take card from deck.
   const card = takeCard();
-  playerScore += cardValue( card );
+  playerScore += cardValue(card);
   // Display score on HTML.
   scores[0].innerText = playerScore;
 
   // Create card and append it to HTML.
-  const cardImg = document.createElement('img');
+  const cardImg = document.createElement("img");
   cardImg.src = `./assets/cards/${card}.png`;
-  cardImg.classList.add('blackjack-card');
-  playerCardsDiv.append( cardImg );
+  cardImg.classList.add("blackjack-card");
+  playerCardsDiv.append(cardImg);
 
   // Check if player has won.
-  if ( playerScore > 21 ) {
-    btnTakeCard.disabled    = true;
+  if (playerScore > 21) {
+    btnTakeCard.disabled = true;
     btnStopPlaying.disabled = true;
-    computersTurn( playerScore );
-  } else if ( playerScore === 21 ) {
-    computersTurn( playerScore );
+    computersTurn(playerScore);
+  } else if (playerScore === 21) {
+    computersTurn(playerScore);
     btnStopPlaying.disabled = true;
   }
 });
 
-btnStopPlaying.addEventListener('click', () => {
-
-  btnTakeCard.disabled    = true;
+btnStopPlaying.addEventListener("click", () => {
+  btnTakeCard.disabled = true;
   btnStopPlaying.disabled = true;
-  computersTurn( playerScore );
+  computersTurn(playerScore);
+});
+
+btnNewGame.addEventListener("click", () => {
+  startGame();
 });
